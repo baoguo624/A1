9ce74ef8979cb5a5708ab15cb57c98a9cc43068d
Guo Shaobin <563273935@qq.com>
2014-11-26 11:25:08 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld{              //the first program.
+  public static void main(String args[])
+  {
+    System.out.println("hello world,i am guoshaobin.i will be a millionaire in the future.");
+  }
+}
